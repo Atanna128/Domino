776959196e7e752089e2c2f631b0315157ee383b
hoang cong thanh <hoangcongthanh1237d@gmail.com>
2020-10-11 11:30:59 +0700

    fix

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
         switch (choice){
             case 1:ConsoleDomino.run();break;
-            case 2:javafx.application.Application.lauch(GUIDomino.class);
+            case 2:javafx.application.Application.launch(GUIDomino.class);
             default:break;
         }
     }
